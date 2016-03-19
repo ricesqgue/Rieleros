5c3c02ae8415eb2201f9829f8f74be411340ef75
Ricardo Esqueda <ricardoeg66@gmail.com>
2016-03-19 08:40:50 -0600

    Parte de historia de Kevin y sonidos en roster

--- js/rieleros.js
+++ js/rieleros.js
@@ -13,12 +13,16 @@ function rotarImg(num){
     fotoJugador.addClass("animated flipOutY");
     setTimeout(
         function(){ 
-        fotoJugador.removeClass("animated flipOutY"); 
-        fotoJugador.addClass("hidden"); 
-        infoJugador.removeClass("hidden"); 
-        infoJugador.addClass("animated flipInY")
-    }, 600);
+            fotoJugador.removeClass("animated flipOutY"); 
+            fotoJugador.addClass("hidden"); 
+            infoJugador.removeClass("hidden"); 
+            infoJugador.addClass("animated flipInY")
+        }, 600);
     setTimeout(function(){infoJugador.removeClass("animated flipInY")},500);
+    document.getElementById("soundPlayer"+num).play();
+    //$("#soundPlayer"+num).attr("autoplay","");
+    //$("#soundPlayer"+num).removeAttr("autoplay");
+
 }
 
 
@@ -29,58 +33,107 @@ function rotarInfo(num){
     infoJugador.addClass("animated flipOutY");
     setTimeout(
         function(){ 
-        infoJugador.removeClass("animated flipOutY"); 
-        infoJugador.addClass("hidden"); 
-        fotoJugador.removeClass("hidden"); 
-        fotoJugador.addClass("animated flipInY")
-    }, 600);
+            infoJugador.removeClass("animated flipOutY"); 
+            infoJugador.addClass("hidden"); 
+            fotoJugador.removeClass("hidden"); 
+            fotoJugador.addClass("animated flipInY")
+        }, 600);
     setTimeout(function(){fotoJugador.removeClass("animated flipInY")},500);
+    document.getElementById("soundPlayer"+num).pause();
+    document.getElementById("soundPlayer"+num).currentTime = 0;
+
 }
 
 
 //Funcion para cambiar entre diferentes divs, recibe el numero de div y la direccion. Son solo 3 divs.. si son más agregar los 'if'.
 function cambiaRoster(num,direccion){
-            if(num==='1'){
-                $("#roster2").addClass("animated fadeOut"+direccion);
-                $("#roster3").addClass("animated fadeOut"+direccion);
-                setTimeout(function(){
-                    $("#roster2").addClass("hidden");
-                    $("#roster3").addClass("hidden");
-                    $("#roster1").removeClass("animated fadeIn");
-                    $("#roster2").removeClass("animated fadeOut"+direccion);
-                    $("#roster3").removeClass("animated fadeOut"+direccion);
-                    $("#roster1").removeClass('hidden');
-                    $("#roster1").addClass("animated fadeIn");
-                },400);
+    if(num==='1'){
+        $("#roster2").addClass("animated fadeOut"+direccion);
+        $("#roster3").addClass("animated fadeOut"+direccion);
+        setTimeout(function(){
+            $("#roster2").addClass("hidden");
+            $("#roster3").addClass("hidden");
+            $("#roster1").removeClass("animated fadeIn");
+            $("#roster2").removeClass("animated fadeOut"+direccion);
+            $("#roster3").removeClass("animated fadeOut"+direccion);
+            $("#roster1").removeClass('hidden');
+            $("#roster1").addClass("animated fadeIn");
+        },400);
 
-            }
-            else if(num==='2'){
+    }
+    else if(num==='2'){
 
-                $("#roster1").addClass("animated fadeOut"+direccion);
-                $("#roster3").addClass("animated fadeOut"+direccion);
-                setTimeout(function(){
-                    $("#roster1").addClass("hidden");
-                    $("#roster3").addClass("hidden");
-                    $("#roster2").removeClass("animated fadeIn");
-                    $("#roster1").removeClass("animated fadeOut"+direccion);
-                    $("#roster3").removeClass("animated fadeOut"+direccion);
-                    $("#roster2").removeClass('hidden');
-                    $("#roster2").addClass("animated fadeIn");                    
-                },400);
-            }
-            else if(num==='3'){
+        $("#roster1").addClass("animated fadeOut"+direccion);
+        $("#roster3").addClass("animated fadeOut"+direccion);
+        setTimeout(function(){
+            $("#roster1").addClass("hidden");
+            $("#roster3").addClass("hidden");
+            $("#roster2").removeClass("animated fadeIn");
+            $("#roster1").removeClass("animated fadeOut"+direccion);
+            $("#roster3").removeClass("animated fadeOut"+direccion);
+            $("#roster2").removeClass('hidden');
+            $("#roster2").addClass("animated fadeIn");                    
+        },400);
+    }
+    else if(num==='3'){
 
-                $("#roster1").addClass("animated fadeOut"+direccion);
-                $("#roster2").addClass("animated fadeOut"+direccion);
-                setTimeout(function(){
-                    $("#roster1").addClass("hidden");
-                    $("#roster2").addClass("hidden");
-                    $("#roster3").removeClass("animated fadeIn");
-                    $("#roster1").removeClass("animated fadeOut"+direccion);
-                    $("#roster2").removeClass("animated fadeOut"+direccion);
+        $("#roster1").addClass("animated fadeOut"+direccion);
+        $("#roster2").addClass("animated fadeOut"+direccion);
+        setTimeout(function(){
+            $("#roster1").addClass("hidden");
+            $("#roster2").addClass("hidden");
+            $("#roster3").removeClass("animated fadeIn");
+            $("#roster1").removeClass("animated fadeOut"+direccion);
+            $("#roster2").removeClass("animated fadeOut"+direccion);
 
-                    $("#roster3").removeClass('hidden');
-                    $("#roster3").addClass("animated fadeIn");
-                },400);
-            }
-        }
+            $("#roster3").removeClass('hidden');
+            $("#roster3").addClass("animated fadeIn");
+        },400);
+    }
+}
+
+function cambiaLinea(num,direccion){
+    if(num==='1'){
+        $("#lineaTiempo2").addClass("animated fadeOut"+direccion);
+        $("#lineaTiempo3").addClass("animated fadeOut"+direccion);
+        setTimeout(function(){
+            $("#lineaTiempo2").addClass("hidden");
+            $("#lineaTiempo3").addClass("hidden");
+            $("#lineaTiempo1").removeClass("animated fadeIn");
+            $("#lineaTiempo2").removeClass("animated fadeOut"+direccion);
+            $("#lineaTiempo3").removeClass("animated fadeOut"+direccion);
+            $("#lineaTiempo1").removeClass('hidden');
+            $("#lineaTiempo1").addClass("animated fadeIn");
+        },400);
+
+    }
+    else if(num==='2'){
+
+        $("#lineaTiempo1").addClass("animated fadeOut"+direccion);
+        $("#lineaTiempo3").addClass("animated fadeOut"+direccion);
+        setTimeout(function(){
+            $("#lineaTiempo1").addClass("hidden");
+            $("#lineaTiempo3").addClass("hidden");
+            $("#lineaTiempo2").removeClass("animated fadeIn");
+            $("#lineaTiempo1").removeClass("animated fadeOut"+direccion);
+            $("#lineaTiempo3").removeClass("animated fadeOut"+direccion);
+            $("#lineaTiempo2").removeClass('hidden');
+            $("#lineaTiempo2").addClass("animated fadeIn");
+        },400);
+    }
+    else if(num==='3'){
+
+        $("#lineaTiempo1").addClass("animated fadeOut"+direccion);
+        $("#lineaTiempo2").addClass("animated fadeOut"+direccion);
+        setTimeout(function(){
+            $("#lineaTiempo1").addClass("hidden");
+            $("#lineaTiempo2").addClass("hidden");
+            $("#lineaTiempo3").removeClass("animated fadeIn");
+            $("#lineaTiempo1").removeClass("animated fadeOut"+direccion);
+            $("#lineaTiempo2").removeClass("animated fadeOut"+direccion);
+
+            $("#lineaTiempo3").removeClass('hidden');
+            $("#lineaTiempo3").addClass("animated fadeIn");
+        },400);
+    }
+}
